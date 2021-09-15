f3a809daae99a80de5e49497a86f5eed71ccf318
Evashna <evashna.pillay@gmail.com>
2021-09-15 20:47:18 +0200

    added editable text fields

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -11,8 +11,7 @@ export class Profile extends Component {
 
     componentDidMount() {
         this.props.fetchUser()
-
-
+    
     }
 
     render() {
@@ -25,13 +24,59 @@ export class Profile extends Component {
         }
         return (
             <View style={{ flex: 1, justifyContent: "center" }}>
+                <View>
+                    <Text> First names: </Text>
+                    <TextInput
+                        placeholder= {currentUser.displayName}
+                        onChangeText= {(val) => this.updateInputVal(val, "displayName")}
+                    />
+                </View>
+                
+                <View>
+                    <Text> Surname: </Text>
+                    <TextInput
+                    placeholder= {currentUser.surname}
+                    onChangeText= {(val) => this.updateInputVal(val, "surname")}
+                    />
+                </View>
 
-                <Text>First Name: {currentUser.displayName}</Text>
-                <Text>Surname: {currentUser.surname}</Text>
+                <View>
+                    <Text> ID Number: </Text>
+                    <TextInput
+                    placeholder= {currentUser.idNumber}
+                    onChangeText= {(val) => this.updateInputVal(val, "idNumber")}
+                    />
+                </View>
+
+                <View>
+                    <Text> Address: </Text>
+                    <TextInput
+                        placeholder= {currentUser.address}
+                        onChangeText= {(val) => this.updateInputVal(val, "Address")}
+                    />
+                </View>
+
+                <View>
+                    <Text> Vaccine Choice: </Text>
+                    <TextInput
+                    placeholder= {currentUser.vaccineChoice}
+                    onChangeText= {(val) => this.updateInputVal(val, "vaccineChoice")}
+                    />
+                </View>
+
+                <View>
+                    <Text> Email: </Text>
+                    <TextInput
+                    placeholder= {currentUser.email}
+                    onChangeText= {(val) => this.updateInputVal(val, "Email Address")}
+                    />
+                </View>
+
+                {/* <Text>Surname: {currentUser.surname}</Text>
                 <Text>ID Number: {currentUser.idNumber}</Text>
                 <Text>Address: {currentUser.address}</Text>
                 <Text>Vaccine Choice: {currentUser.vaccineChoice} </Text>
-                <Text>Email: {currentUser.email}</Text>
+                <Text>Email: {currentUser.email}</Text> */}
 
                 <Button
                     color="#3740FE"
