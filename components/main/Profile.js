00ce6e192995f657d30b67bd1ee5080200bb02f3
TPhaahla <72293519+TPhaahla@users.noreply.github.com>
2021-09-17 18:42:17 +0200

    revert 'Profile.js'

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -7,90 +7,43 @@ import MainS from '../Main';
 import firebase from 'firebase'
 require('firebase/firestore')
 
-function Profile(props) {
+export class Profile extends Component {
+
+    componentDidMount() {
+        this.props.fetchUser()
 
 
-    const { currentUser } = props;
-
-    if (currentUser == undefined) {
-        return (
-            <View></View>
-        )
     }
-    return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.Text}> First names: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.displayName}
-                    onChangeText={(val) => this.updateInputVal(val, "displayName")}
-                />
-            </View>
 
-            <View>
-                <Text style={styles.Text}> Surname: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.surname}
-                    onChangeText={(val) => this.updateInputVal(val, "surname")}
-                />
-            </View>
+    render() {
+        const { currentUser } = this.props;
 
-            <View>
-                <Text style={styles.Text}> ID Number: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.idNumber}
-                    onChangeText={(val) => this.updateInputVal(val, "idNumber")}
-                />
-            </View>
+        if (currentUser == undefined) {
+            return (
+                <View></View>
+            )
+        }
+        return (
+            <View style={{ flex: 1, justifyContent: "center" }}>
 
-            <View>
-                <Text style={styles.Text}> Address: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.address}
-                    onChangeText={(val) => this.updateInputVal(val, "Address")}
-                />
-            </View>
-
-            <View>
-                <Text style={styles.Text}> Vaccine Choice: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.vaccineChoice}
-                    onChangeText={(val) => this.updateInputVal(val, "vaccineChoice")}
-                />
-            </View>
-
-            <View>
-                <Text style={styles.Text} > Email: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.email}
-                    onChangeText={(val) => this.updateInputVal(val, "Email Address")}
-                />
-            </View>
-
-            {/* <Text>Surname: {currentUser.surname}</Text>
+                <Text>First Name: {currentUser.displayName}</Text>
+                <Text>Surname: {currentUser.surname}</Text>
                 <Text>ID Number: {currentUser.idNumber}</Text>
                 <Text>Address: {currentUser.address}</Text>
                 <Text>Vaccine Choice: {currentUser.vaccineChoice} </Text>
-                <Text>Email: {currentUser.email}</Text> */}
+                <Text>Email: {currentUser.email}</Text>
 
-            <Button
-                //color="#3740FE"
-                color="#1E90FF"
-                title="Log Out"
-                onPress={() => onLogout()}
-            />
+                <Button
+                    color="#3740FE"
+                    title="Log Out"
+                    onPress={() => onLogout()}
+                />
 
 
-        </View>
-    )
+            </View>
+        )
+    }
 }
-
 const onLogout = () => {
     firebase.auth().signOut();
 }
@@ -101,49 +54,7 @@ const mapStateToProps = (store) => ({
 
 })
 
+const mapDispatchProps = (dispatch) => bindActionCreators({ fetchUser }, dispatch);
 
-
-
-const styles = StyleSheet.create({
-
-    inputStyle: {
-        //flex: 1,
-        marginLeft: 30,
-        marginBottom: 10,
-        padding: 10,
-        alignSelf: "center",
-        borderColor: "#ccc",
-        borderBottomWidth: 1,
-        //width: 100,
-    },
-
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        //alignItems: "center",
-        justifyContent: "center",
-    },
-
-
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
-        marginLeft: 20,
-    },
-
-    Text: {
-        //color: "#1E90FF", //light blue
-        color: "black",
-        fontWeight: "bold",
-        fontSize: 12,
-        height: 15,
-        flex: 1,
-        padding: 20,
-        marginLeft: 20,
-
-    }
-});
-
-export default connect(mapStateToProps, null)(Profile);
+export default connect(mapStateToProps, mapDispatchProps)(Profile);
 //export default Profile;
