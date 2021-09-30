317eeb1138e97289ca2b941ad8b219eb6197aaa6
Evashna <evashna.pillay@gmail.com>
2021-10-01 01:19:13 +0200

    added save button

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { fetchUser } from '../../redux/actions/index';
 import MainS from '../Main';
 import firebase from 'firebase'
+import { render } from "react-dom";
 require('firebase/firestore')
 //
 function Profile(props) {
@@ -17,74 +18,88 @@ function Profile(props) {
             <View></View>
         )
     }
+
     return (
         <View style={styles.container}>
-            <View>
+            
                 <Text style={styles.Text}> First names: </Text>
                 <TextInput
                     style={styles.inputStyle}
                     placeholder={currentUser.displayName}
                     onChangeText={(val) => this.updateInputVal(val, "displayName")}
                 />
-            </View>
+           
 
-            <View>
+            
                 <Text style={styles.Text}> Surname: </Text>
                 <TextInput
                     style={styles.inputStyle}
                     placeholder={currentUser.surname}
                     onChangeText={(val) => this.updateInputVal(val, "surname")}
                 />
-            </View>
+           
 
-            <View>
+           
                 <Text style={styles.Text}> ID Number: </Text>
                 <TextInput
                     style={styles.inputStyle}
                     placeholder={currentUser.idNumber}
                     onChangeText={(val) => this.updateInputVal(val, "idNumber")}
                 />
-            </View>
+            
 
-            <View>
+           
                 <Text style={styles.Text}> Address: </Text>
                 <TextInput
                     style={styles.inputStyle}
                     placeholder={currentUser.address}
                     onChangeText={(val) => this.updateInputVal(val, "Address")}
                 />
-            </View>
+           
 
-            <View>
+           
                 <Text style={styles.Text}> Vaccine Choice: </Text>
                 <TextInput
                     style={styles.inputStyle}
                     placeholder={currentUser.vaccineChoice}
                     onChangeText={(val) => this.updateInputVal(val, "vaccineChoice")}
                 />
-            </View>
+           
 
-            <View>
+            
                 <Text style={styles.Text} > Email: </Text>
                 <TextInput
                     style={styles.inputStyle}
                     placeholder={currentUser.email}
                     onChangeText={(val) => this.updateInputVal(val, "Email Address")}
                 />
-            </View>
+           
 
-
-            <Button
-
+           <View style={styles.cont}>
+            < View style= {styles.btn}>
+            
+            <Button  
                 color="#1E90FF"
+                //color="#DCDCDC" 
+                //color="#99ccff"
+                title="Save"
+                onPress={() => mapStateToProps()}
+                //onPress={() => this.buttonClickListener()}
+            />
+           
+            <Button
+                color="#DCDCDC"
                 title="Log Out"
                 onPress={() => onLogout()}
             />
-
+            
+            </View>
+            </View>
 
         </View>
     )
 }
+
 
 const onLogout = () => {
     firebase.auth().signOut();
@@ -96,48 +111,56 @@ const mapStateToProps = (store) => ({
 
 })
 
-
-
-
 const styles = StyleSheet.create({
 
     inputStyle: {
 
         marginLeft: 30,
         marginBottom: 10,
-        padding: 10,
+        padding: 5,
         alignSelf: "center",
         borderColor: "#ccc",
         borderBottomWidth: 1,
+        width: '30%',
 
     },
 
     container: {
         flex: 1,
         backgroundColor: "#fff",
-
         justifyContent: "center",
     },
 
-
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
-        marginLeft: 20,
+    cont: {
+        flex: 1, 
+        flexDirection: 'row',
+        justifyContent: 'flex-end',  
     },
 
-    Text: {
+    btn: {
+        width: '20%',
+        height: 30,
+        //alignItems: 'center',
 
+    },
+
+    // TextInput: {
+    //     height: 20,
+    //     flex: 1,
+    //     padding: 5,
+    //     marginLeft: 20,
+    // },
+
+    Text: {
         color: "black",
         fontWeight: "bold",
         fontSize: 12,
-        height: 15,
-        flex: 1,
+        height: 8,
         padding: 20,
         marginLeft: 20,
 
-    }
+    },
+
 });
 
 export default connect(mapStateToProps, null)(Profile);
