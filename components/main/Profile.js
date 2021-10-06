3e265cf152f53ae8afac8cdea6f87cd3244012a6
Evashna <evashna.pillay@gmail.com>
2021-10-06 16:11:08 +0200

    commit changes

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import { View, TextInput, StyleSheet, Button, Text } from "react-native";
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux';
-import { fetchUser } from '../../redux/actions/index';
-import MainS from '../Main';
-import firebase from 'firebase'
-import { render } from "react-dom";
-require('firebase/firestore')
-//
-=======
 import React, { Component, useState } from "react";
 import {
   View,
@@ -31,7 +19,6 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 require("firebase/firestore");
 
->>>>>>> 9b155a3f23dc312b2a6f95fc245c3cb2bb0ad04b
 function Profile(props) {
   const { currentUser } = props;
 
@@ -41,90 +28,6 @@ function Profile(props) {
   const [checkedJJ, setCheckedJJ] = useState(currentUser.checkedJJ);
   const [address, setAddress] = useState(currentUser.address);
 
-<<<<<<< HEAD
-    if (currentUser == undefined) {
-        return (
-            <View></View>
-        )
-    }
-
-    return (
-        <View style={styles.container}>
-            
-                <Text style={styles.Text}> First names: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.displayName}
-                    onChangeText={(val) => this.updateInputVal(val, "displayName")}
-                />
-           
-
-            
-                <Text style={styles.Text}> Surname: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.surname}
-                    onChangeText={(val) => this.updateInputVal(val, "surname")}
-                />
-           
-
-           
-                <Text style={styles.Text}> ID Number: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.idNumber}
-                    onChangeText={(val) => this.updateInputVal(val, "idNumber")}
-                />
-            
-
-           
-                <Text style={styles.Text}> Address: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.address}
-                    onChangeText={(val) => this.updateInputVal(val, "Address")}
-                />
-           
-
-           
-                <Text style={styles.Text}> Vaccine Choice: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.vaccineChoice}
-                    onChangeText={(val) => this.updateInputVal(val, "vaccineChoice")}
-                />
-           
-
-            
-                <Text style={styles.Text} > Email: </Text>
-                <TextInput
-                    style={styles.inputStyle}
-                    placeholder={currentUser.email}
-                    onChangeText={(val) => this.updateInputVal(val, "Email Address")}
-                />
-           
-
-           <View style={styles.cont}>
-            < View style= {styles.btn}>
-            
-            <Button  
-                color="#1E90FF"
-                //color="#DCDCDC" 
-                //color="#99ccff"
-                title="Save"
-                //onPress={() => mapStateToProps()}
-                //onPress={() => this.buttonClickListener()}
-            />
-           
-            <Button
-                color="#DCDCDC"
-                title="Log Out"
-                onPress={() => onLogout()}
-            />
-            
-            </View>
-            </View>
-=======
   // var checkedPfizer = currentUser.checkedPfizer;
   // var checkedJJ = currentUser.checkedJJ;
 
@@ -133,7 +36,6 @@ function Profile(props) {
   }
   if (editFlag == false) {
     // console.log(firebase.auth.currentUser.uid);
->>>>>>> 9b155a3f23dc312b2a6f95fc245c3cb2bb0ad04b
 
     return (
       <View style={styles.container}>
@@ -289,63 +191,6 @@ const onUpdate = (newAddress, JJ, Pfizer) => {
 };
 
 const mapStateToProps = (store) => ({
-<<<<<<< HEAD
-
-    currentUser: store.userState.currentUser
-
-})
-
-const styles = StyleSheet.create({
-
-    inputStyle: {
-
-        marginLeft: 30,
-        marginBottom: 10,
-        padding: 5,
-        alignSelf: "center",
-        borderColor: "#ccc",
-        borderBottomWidth: 1,
-        width: '30%',
-
-    },
-
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        justifyContent: "center",
-    },
-
-    cont: {
-        flex: 1, 
-        flexDirection: 'row',
-        justifyContent: 'flex-end',  
-    },
-
-    btn: {
-        width: '20%',
-        height: 30,
-        //alignItems: 'center',
-
-    },
-
-    // TextInput: {
-    //     height: 20,
-    //     flex: 1,
-    //     padding: 5,
-    //     marginLeft: 20,
-    // },
-
-    Text: {
-        color: "black",
-        fontWeight: "bold",
-        fontSize: 12,
-        height: 8,
-        padding: 20,
-        marginLeft: 20,
-
-    },
-
-=======
   currentUser: store.userState.currentUser,
 });
 
@@ -393,7 +238,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     backgroundColor: "#427bd2",
   },
->>>>>>> 9b155a3f23dc312b2a6f95fc245c3cb2bb0ad04b
 });
 
 export default connect(mapStateToProps, null)(Profile);
